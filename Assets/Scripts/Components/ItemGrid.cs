7c0b2742ca51ea3065cc507a5ed6947742d6ff59
zoniventris <f.zoniventris@gmail.com>
2021-11-26 15:21:51 -0800

    Implement basic selection mechanics.

---
+++ Assets/Scripts/Components/ItemGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class ItemGrid : MonoBehaviour
+{
+    public Vector2Int size;
+    public List<TileBase> itemTiles = new List<TileBase>();
+
+    private Tilemap tilemap;
+
+    private void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        PopulateItems();
+    }
+
+    private void PopulateItems()
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                int tileIndex = Random.Range(0, itemTiles.Count);
+                tilemap.SetTile(new Vector3Int(x, y, 0), itemTiles[tileIndex]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Components/PlayerInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Tilemap itemTilemap;
+    public Tilemap indicatorTilemap;
+
+    public TileBase selectionIndicator;
+
+    private List<Vector3Int> selectedCells = new List<Vector3Int>();
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Select"))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int mouseCell = itemTilemap.WorldToCell(mousePos);
+
+            TileBase indicatorAtPosition = indicatorTilemap.GetTile(mouseCell);
+            if (indicatorAtPosition == selectionIndicator)
+            {
+                indicatorTilemap.SetTile(mouseCell, null);
+                selectedCells.Remove(mouseCell);
+            }
+            else
+            {
+                if (selectedCells.Count == 0)
+                {
+                    indicatorTilemap.SetTile(mouseCell, selectionIndicator);
+                    selectedCells.Add(mouseCell);
+                }
+                else
+                {
+                    bool isValid = false;
+                    foreach (Vector3Int cell in selectedCells)
+                    {
+                        int xDiff = Mathf.Abs(cell.x - mouseCell.x);
+                        int yDiff = Mathf.Abs(cell.y - mouseCell.y);
+
+                        if (xDiff <= 1 && yDiff <= 1 && (xDiff == 0 ^ yDiff == 0))
+                        {
+                            isValid = true;
+                            break;
+                        }
+
+                    }
+
+                    if (isValid)
+                    {
+                        indicatorTilemap.SetTile(mouseCell, selectionIndicator);
+                        selectedCells.Add(mouseCell);
+                    }
+                }
+            }
+        }
+    }
+}
