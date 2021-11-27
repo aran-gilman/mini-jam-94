ac31334c19eeb8ac1e4996485e29f4001e13a87e
zoniventris <f.zoniventris@gmail.com>
2021-11-26 23:31:40 -0800

    Tweak reciple list layout.

---
+++ Assets/Scripts/Components/RecipeList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeList : MonoBehaviour
+{
+    public GameObject prefab;
+    public int spacing;
+
+    private void Start()
+    {
+        Recipe[] recipes = Resources.LoadAll<Recipe>("Recipes");
+        int y = 0;
+        foreach (Recipe recipe in recipes)
+        {
+            GameObject go = Instantiate(prefab, transform);
+            go.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, y, spacing);
+            go.GetComponent<RecipeDisplay>().recipe = recipe;
+            y += spacing;
+        }
+    }
+}
