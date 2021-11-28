60bf5672bc2ad26048c8d62fddaadaed07f217a6
zoniventris <f.zoniventris@gmail.com>
2021-11-27 18:46:16 -0800

    Add intro scene with instructions.

---
+++ Assets/Scripts/Components/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string nextScene;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Select"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
